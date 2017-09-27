5206561ed74b8f297a8def2d707fe07091fac1a2
Theo-denBlanken <t.denblanken@ma-web.nl>
2017-09-27 21:43:08 +0200

    sticky navigatie

--- scripts/activeLink.js
+++ scripts/activeLink.js
@@ -16,3 +16,36 @@ for (let i=0; i<linkjes.length; i++) {
     zetActiveOm(this);
   })
 }
+
+// positie van de navigatieBalk
+// de navigatieBalk pakken
+const navigatieBalk = document.getElementById('nav');
+
+// de verschuiving van de navigatiebalk bepalen
+let navPositie = navigatieBalk.offsetTop;
+let voormaligeScrollWaarde = 0; //de laatste scrollPosiite van het venster
+
+// ga na of de naviagtie moet bewegen
+const kleefNav = () => {
+  if(window.scrollY >= navPositie) {
+    schuifNav();
+  } else {
+    // alle eventueel toegevoegde klasnamen verwijderen
+    navigatieBalk.classList.remove('kleven');
+    navigatieBalk.classList.remove('verbergen');
+  }
+  voormaligeScrollWaarde = window.scrollY;
+}
+const schuifNav = () => {
+  console.log('de balk moet schuiven');
+  if ( window.scrollY < voormaligeScrollWaarde ) {
+    // boven in venster schuiven
+    navigatieBalk.classList.remove('verbergen');
+    navigatieBalk.classList.add('kleven');
+  } else {
+    // uit het venster schuiven
+    navigatieBalk.classList.remove('kleven');
+    navigatieBalk.classList.add('verbergen');
+  }
+}
+window.addEventListener('scroll', kleefNav, false);
